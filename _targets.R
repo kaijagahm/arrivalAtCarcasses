024910084c9d7b390ac88e3bde033de0ad4cb96e
Kaija Gahm <kaija.gahm@gmail.com>
2024-09-10 18:32:07 -0700

    implement dynamic branching for about half the pipeline

--- _targets.R
+++ _targets.R
@@ -11,6 +11,7 @@ library(targets)
 tar_option_set(
   memory = "transient", 
   garbage_collection = TRUE,
+  controller = crew::crew_controller_local(workers = 2),
   format = "qs",
   error = "null",
   # seconds_meta_append = 15,
@@ -57,72 +58,87 @@ list(
   tar_target(data_files_2024, list.files(here("data/ACC/2024_hf_period/raw/"), pattern = ".csv", full.names = T)),
   tar_target(unobs_raw_acc_2024, purrr::list_rbind(map(data_files_2024, ~as.data.frame(data.table::fread(.x, select = c("Latitude", "Longitude", "UTC_datetime", "UTC_date", "UTC_time", "datatype", "device_id", "acc_x", "acc_y", "acc_z")))))),
   tar_target(split, group_split(unobs_raw_acc_2024 %>% group_by(device_id))),
-  tar_target(written_out, for(i in 1:length(split)){
-    dev <- split[[i]]$device_id[1]  
-    filename <- paste0("/", dev, ".csv")
-    data.table::fwrite(split[[i]], 
-                       file = paste0(here("data/ACC/2024_hf_period/created/devices"), 
-                                     filename))
-  }),
-  tar_target(split_files_2024, list.files(here("data/ACC/2024_hf_period/created/devices/"), pattern = ".csv", full.names = T)),
-  #tar_target(split_subset, split[6:7]),
+  # tar_target(written_out, for(i in 1:length(split)){
+  #   dev <- split[[i]]$device_id[1]  
+  #   filename <- paste0("/", dev, ".csv")
+  #   data.table::fwrite(split[[i]], 
+  #                      file = paste0(here("data/ACC/2024_hf_period/created/devices"), 
+  #                                    filename))
+  # }),
+  # tar_target(split_files_2024, list.files(here("data/ACC/2024_hf_period/created/devices/"), pattern = ".csv", full.names = T)),
+  tar_target(split_subset, split[6:8], iteration = "list"),
   ### Classify data
-  tar_target(prepared_2024, prepare_forloop(split, cal = calibration_data)),
-  tar_target(bouts_2024, purrr::map(prepared_2024, ~{
-    .x %>%
-      dplyr::select(bout_id, device_id, start_int) %>%
-      dplyr::group_by(device_id, bout_id) %>%
-      dplyr::summarize(start = min(start_int),
-                       end = max(start_int)) %>%
-      dplyr::ungroup()
-  })),
-  tar_target(predictions_2024, purrr::map(prepared_2024, 
-                                          ~stats::predict(gv_model, .x))),
-  tar_target(scores_2024, purrr::map(prepared_2024, 
-                                     ~stats::predict(gv_model, .x, type = "prob"))),
-  tar_target(bouts_predictions_2024, purrr::pmap(.l = list(prepared_2024, predictions_2024, scores_2024, bouts_2024), ~get_bouts_predictions(..1, ..2, ..3, ..4))),
-  tar_target(ornitela_data_2024, 
+  tar_target(prepared_2024, prepare_dataset(split_subset, calibration = calibration_data),
+             pattern = map(split_subset), iteration = "list"),
+  tar_target(bouts_2024, prepared_2024[,c("bout_id", "device_id", "start_int")] %>%
+               group_by(device_id, bout_id) %>%
+               summarize(start = min(start_int),
+                         end = max(start_int),
+                         .groups = "drop"),
+             pattern = map(prepared_2024),
+             iteration = "list"), # XXX this works, but it shouldn't be so slow! Why is this so slow??
+  tar_target(predictions_2024, predict(gv_model, prepared_2024),
+             pattern = map(prepared_2024),
+             iteration = "list"),
+  tar_target(scores_2024, predict(gv_model, prepared_2024, type = "prob"),
+             pattern = map(prepared_2024),
+             iteration = "list"),
+  tar_target(bouts_predictions_2024, 
+             get_bouts_predictions(prepared_2024,
+                                   predictions_2024,
+                                   scores_2024,
+                                   bouts_2024),
+             pattern = map(prepared_2024, 
+                           predictions_2024, 
+                           scores_2024, 
+                           bouts_2024),
+             iteration = "list"),
+  tar_target(bouts_predictions_2024_distinct, distinct(bouts_predictions_2024),
+             pattern = map(bouts_predictions_2024),
+             iteration = "list"),
+  tar_target(ornitela_data_2024,
              vultureUtils::downloadVultures(loginObject = loginObject,
                                             removeDup = T, dfConvert = T,
                                             quiet = T,
-                                            dateTimeStartUTC = 
+                                            dateTimeStartUTC =
                                               lubridate::ymd_hms(min(unobs_raw_acc_2024$UTC_datetime)),
-                                            dateTimeEndUTC = 
+                                            dateTimeEndUTC =
                                               lubridate::ymd_hms(max(unobs_raw_acc_2024$UTC_datetime)))),
   tar_target(gps, dplyr::select(ornitela_data_2024, tag_id, timestamp, dateOnly, ground_speed, location_lat, location_long, individual_id, tag_local_identifier)),
-  tar_target(device_ids, purrr::map_dbl(bouts_predictions_2024, ~.x$device_id[1])),
-  tar_target(focal_gps, purrr::map(device_ids, ~gps %>%
-                                     filter(tag_local_identifier == .x))),
-  tar_target(bouts_predictions_2024_distinct, map(bouts_predictions_2024, distinct)),
-  tar_target(matches, map2(bouts_predictions_2024_distinct, focal_gps,
-                           ~get_matches(df = .x, foc = .y))),
-  tar_target(joined, map2(bouts_predictions_2024_distinct, matches, ~{
-    left_join(.x, .y, by = c("device_id" = "tag_local_identifier",
-                             "bout_id"))
-  })),
-  ### Get feeding bouts
-  tar_target(feeding_bouts_certain, map(joined, ~filter(.x, pred == "Eating" & 
-                                                          !is.na(location_lat) &
-                                                          .pred_Eating > 0.5) %>% 
-                                          sf::st_as_sf(coords = c("location_long", "location_lat"), crs = "WGS84"))),
-  ### Classify feeding station vs. not
-  tar_target(fs, readxl::read_excel(here("data/FeedingData from 2018_2024_Translated.xlsx")) %>%
-               dplyr::select(contains("LONG") | contains("LAT")) %>%
-               rename("itmLong" = `ITM - LONG`,
-                      "itmLat" = `ITM - LAT`,
-                      "long" = `WGS84 - LONG`,
-                      "lat" = `WGS84 - LAT`) %>%
-               select(long, lat, itmLong, itmLat) %>%
-               distinct() %>%
-               st_as_sf(coords = c("long", "lat"), crs = "WGS84") %>%
-               st_transform(32636)),
-  tar_target(fs_buffered, sf::st_buffer(fs, dist = 100)),
-  tar_target(fs_union, sf::st_union(fs_buffered) %>% st_transform("WGS84")),
-  tar_target(feeding_bouts_station, map(feeding_bouts_certain, ~{
-    .x$station <- !is.na(as.numeric(st_intersects(.x, fs_union)))
-    return(.x)
-  }))
-  # XXX START HERE--need to properly clean the GPS data before classifying it.
+  tar_target(device_ids, purrr::map(bouts_predictions_2024, ~.x$device_id[1]),
+             iteration = "list"),
+  tar_target(focal_gps, filter(gps, tag_local_identifier == device_ids),
+             pattern = map(device_ids),
+             iteration = "list")#,
+  # tar_target(matches, map2(bouts_predictions_2024_distinct, focal_gps,
+  #                          ~get_matches(df = .x, foc = .y))),
+  # tar_target(joined, map2(bouts_predictions_2024_distinct, matches, ~{
+  #   left_join(.x, .y, by = c("device_id" = "tag_local_identifier",
+  #                            "bout_id"))
+  # })),
+  # ### Get feeding bouts
+  # tar_target(feeding_bouts_certain, map(joined, ~filter(.x, pred == "Eating" & 
+  #                                                         !is.na(location_lat) &
+  #                                                         .pred_Eating > 0.5) %>% 
+  #                                         sf::st_as_sf(coords = c("location_long", "location_lat"), crs = "WGS84"))),
+  # ### Classify feeding station vs. not
+  # tar_target(fs, readxl::read_excel(here("data/FeedingData from 2018_2024_Translated.xlsx")) %>%
+  #              dplyr::select(contains("LONG") | contains("LAT")) %>%
+  #              rename("itmLong" = `ITM - LONG`,
+  #                     "itmLat" = `ITM - LAT`,
+  #                     "long" = `WGS84 - LONG`,
+  #                     "lat" = `WGS84 - LAT`) %>%
+  #              select(long, lat, itmLong, itmLat) %>%
+  #              distinct() %>%
+  #              st_as_sf(coords = c("long", "lat"), crs = "WGS84") %>%
+  #              st_transform(32636)),
+  # tar_target(fs_buffered, sf::st_buffer(fs, dist = 100)),
+  # tar_target(fs_union, sf::st_union(fs_buffered) %>% st_transform("WGS84")),
+  # tar_target(feeding_bouts_station, map(feeding_bouts_certain, ~{
+  #   .x$station <- !is.na(as.numeric(st_intersects(.x, fs_union)))
+  #   return(.x)
+  # }))
+  # # XXX START HERE--need to properly clean the GPS data before classifying it.
 )
 
 
