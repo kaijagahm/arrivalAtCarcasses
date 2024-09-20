f0634cedc04914d455c4a46e440ff96d73f41943
Kaija Gahm <kaija.gahm@gmail.com>
2024-09-20 10:34:42 -0700

    Completed draft of non-targets bout assignment script for 2023 data

--- test.R
+++ test.R
@@ -11,65 +11,166 @@ library(caret)
 library(zoo)
 source(here("R/functions.R"))
 
-data_files_2023 <- list.files(here("data/ACC/2023_hf_period/raw/"), full.names = T)
+# data_files_2023 <- list.files(here("data/ACC/2023_hf_period/raw/"), full.names = T)
+# 
+# unobs_raw_acc_2023 <- data.table::fread(data_files_2023[1], select = c("Latitude", "Longitude", "UTC_datetime", "UTC_date", "UTC_time", "datatype", "device_id", "acc_x", "acc_y", "acc_z"))
+# 
+# splitup <- group_split(group_by(as.data.frame(unobs_raw_acc_2023)), device_id)
+# for(i in 1:length(splitup)){
+#   write_csv(splitup[[i]], file = paste0(here("data/ACC/2023_hf_period/created/"), "/device_", i, ".csv"))
+#   cat("finished", i)
+# }
+# devices <- map_chr(splitup, ~.x$device_id[1])
+#
+# write_rds(unobs_raw_acc_2023, file = here("data/ACC/2023_hf_period/created/unobs_raw_acc_2023.RDS"))
+# unobs_raw_acc_2023 <- readRDS(here("data/ACC/2023_hf_period/created/unobs_raw_acc_2023.RDS"))
+mindate <- lubridate::ymd_hms(min(unobs_raw_acc_2023$UTC_datetime))
+maxdate <- lubridate::ymd_hms(max(unobs_raw_acc_2023$UTC_datetime))
 
-unobs_raw_acc_2023 <- data.table::fread(data_files_2023[1], select = c("Latitude", "Longitude", "UTC_datetime", "UTC_date", "UTC_time", "datatype", "device_id", "acc_x", "acc_y", "acc_z"))
 
-a_2023 <- split_data_fun_forloop(unobs_raw_acc_2023)
+#rm(unobs_raw_acc_2023)
+#rm(splitup)
 
-calibration_data <- read_csv(here("ACC_algo_Marta_draft/Data/example_calibration.csv"))
+# calibration_data <- read_csv(here("ACC_algo_Marta_draft/Data/example_calibration.csv"))
+# 
+# files <- list.files(here("data/ACC/2023_hf_period/created/"), pattern = ".csv", full.names = T)
+# 
+# for(i in 1:length(files)){
+#   name <- str_extract(files[i], "device_[0-9]+")
+#   file <- as.data.frame(data.table::fread(files[i]))
+#   prepared <- prepare_dataset(file, calibration = calibration_data)
+#   write_csv(prepared, paste0(here("data/ACC/2023_hf_period/created/prepared/"), "/", name, "_prepared.csv"))
+#   rm(file)
+#   rm(prepared)
+#   cat("done with", i, "\n")
+# }
+# 
+files <- list.files(here("data/ACC/2023_hf_period/created/prepared/"), pattern = ".csv", full.names = T)
 
-prepared_2023 <- map(a_2023, ~prepare_dataset(.x, calibration = calibration_data),
-                     .progress = T)
+# prepared_2023 <- vector(mode = "list", length = length(files))
+# for(i in 1:length(files)){
+#   prepared_2023[[i]] <- as.data.frame(data.table::fread(files[i]))
+# }
+#write_rds(prepared_2023, here("data/ACC/2023_hf_period/created/prepared_2023.RDS"))
 
-bouts_2023 <- map(prepared_2023, ~{
-  .x[,c("bout_id", "device_id", "start_int")] %>%
+bouts_2023 <- vector(mode = "list", length = length(files))
+
+for(i in 1:length(files)){
+  file <- as.data.frame(data.table::fread(files[i]))
+  bouts_2023[[i]] <- file[,c("bout_id", "device_id", "start_int")] %>%
     group_by(device_id, bout_id) %>%
     summarize(start = min(start_int),
               end = max(start_int),
               .groups = "drop")
+  cat("Done with bouts for", i, "\n")
+}
+
+mod <- readRDS(here("ACC_algo_Marta_draft/Data/gv_final_model_fit.rda"))
+
+predictions_2023 <- vector(mode = "list", length = length(files))
+scores_2023 <- vector(mode = "list", length = length(files))
+for(i in 25:length(files)){
+  file <- as.data.frame(data.table::fread(files[i]))
+  if(nrow(file) > 0){
+    file$start_int <- as.character(file$start_int)
+    predictions_2023[[i]] <- predict(mod, file)
+    scores_2023[[i]] <- predict(mod, file, type = "prob")
+  }
+  cat("Done with predictions and scores for", i, "\n")
+}
+
+write_rds(predictions_2023, file = here("data/ACC/2023_hf_period/created/predictions_2023.RDS"))
+write_rds(scores_2023, file = here("data/ACC/2023_hf_period/created/scores_2023.RDS"))
+write_rds(bouts_2023, file = here("data/ACC/2023_hf_period/created/bouts_2023.RDS"))
+
+predictions_2023 <- readRDS(here("data/ACC/2023_hf_period/created/predictions_2023.RDS"))
+bouts_2023 <- readRDS(here("data/ACC/2023_hf_period/created/bouts_2023.RDS"))
+scores_2023 <- readRDS(here("data/ACC/2023_hf_period/created/scores_2023.RDS"))
+
+prepared_files <- list.files(here("data/ACC/2023_hf_period/created/prepared/"), pattern = ".csv", full.names = T)
+bouts_predictions_2023 <- vector(mode = "list", length = length(predictions_2023))
+for(i in 1:length(prepared_files)){
+  filename <- prepared_files[i]
+  prepared <- as.data.frame(data.table::fread(filename))
+  if(nrow(prepared) > 0){
+    bouts_predictions_2023[[i]] <- distinct(get_bouts_predictions(prepared, predictions_2023[[i]], scores_2023[[i]], bouts_2023[[i]]))
+  }else{
+    bouts_predictions_2023[[i]] <- NULL
+  }
+  
+  cat("finished", i, "\n")
+}
+
+write_rds(bouts_predictions_2023, file = here("data/ACC/2023_hf_period/created/bouts_predictions_2023.RDS"))
+
+# targets::tar_load(loginObject)
+# ornitela_data_2023 <- vultureUtils::downloadVultures(loginObject = loginObject,
+#                                           removeDup = T, dfConvert = T,
+#                                           quiet = T,
+#                                           dateTimeStartUTC = mindate,
+#                                           dateTimeEndUTC = maxdate)
+# 
+# gps_2023 <- dplyr::select(ornitela_data_2023, tag_id, timestamp, dateOnly, ground_speed, location_lat, location_long, individual_id, tag_local_identifier)
+# rm(ornitela_data_2023)
+# gc()
+# 
+# data.table::fwrite(gps_2023, file = here("data/ACC/2023_hf_period/created/gps_2023.csv"))
+gps_2023 <- data.table::fread(here("data/ACC/2023_hf_period/created/gps_2023.csv"))
+
+bouts_predictions_2023 <- readRDS(here("data/ACC/2023_hf_period/created/bouts_predictions_2023.RDS"))
+device_ids_2023 <- purrr::map(bouts_predictions_2023, ~.x$device_id[1])
+
+# focal_gps_2023 <- map(device_ids_2023, ~{
+#   if(length(.x) > 0){
+#     filter(gps_2023, tag_local_identifier == .x)
+#   }else{
+#     NULL
+#   }})
+# write_rds(focal_gps_2023, here("data/ACC/2023_hf_period/created/focal_gps_2023.RDS"))
+focal_gps_2023 <- readRDS(here("data/ACC/2023_hf_period/created/focal_gps_2023.RDS"))
+
+matches_2023 <- map2(bouts_predictions_2023, focal_gps_2023, ~{
+  if(!is.null(.x)){
+    get_matches(.x, .y)
+  }else{
+    NULL
+  }
 }, .progress = T)
+write_rds(matches_2023, here("data/ACC/2023_hf_period/created/matches_2023.RDS"))
+matches_2023 <- readRDS(here("data/ACC/2023_hf_period/created/matches_2023.RDS"))
+# XXX start here
 
-load(here("ACC_algo_Marta_draft/Data/gv_final_model_fit.rda"))
+joined_2023 <- map2(bouts_predictions_2023, matches_2023, ~{
+  if(!is.null(.y)){
+    left_join(.x, .y, by = c("device_id" = "tag_local_identifier",
+                             "bout_id"))
+  }else{
+    NULL
+  }
+})
+write_rds(joined_2023, here("data/ACC/2023_hf_period/created/joined_2023.RDS"))
+joined_2023 <- readRDS(here("data/ACC/2023_hf_period/created/joined_2023.RDS"))
 
-predictions_2023 <- map(prepared_2023, ~predict(gv_final_model_fit, .x),
-                        .progress = T)
+feeding_bouts_certain_2023 <- map(joined_2023, ~{
+  if(!is.null(.x)){
+    filter(.x, pred == "Eating" & !is.na(location_lat) &
+             .pred_Eating > 0.5) %>%
+      sf::st_as_sf(coords = c("location_long", "location_lat"),
+                   crs = "WGS84")
+  }else{
+    NULL
+  }
+})
+write_rds(feeding_bouts_certain_2023, here("data/ACC/2023_hf_period/created/feeding_bouts_certain_2023.RDS"))
+feeding_bouts_certain_2023 <- readRDS(here("data/ACC/2023_hf_period/created/feeding_bouts_certain_2023.RDS"))
 
-scores_2023 <- map(prepared_2023, ~predict(gv_final_model_fit, .x, type = "prob"), .progress = T)
+tar_load(fs_union)
 
-bouts_predictions_2023 <- pmap(list(prepared_2023, predictions_2023, scores_2023, bouts_2023), ~{distinct(get_bouts_predictions(..1, ..2, ..3, ..4))})
+feeding_bouts_station_2023 <- map(feeding_bouts_certain_2023, ~{
+  if(!is.null(.x)){
+    assign_fs(.x, fs_union)
+  }else{NULL}
+})
 
-#####
-tar_target(ornitela_data_2023,
-           vultureUtils::downloadVultures(loginObject = loginObject,
-                                          removeDup = T, dfConvert = T,
-                                          quiet = T,
-                                          dateTimeStartUTC =
-                                            lubridate::ymd_hms(min(unobs_raw_acc_2023$UTC_datetime)),
-                                          dateTimeEndUTC =
-                                            lubridate::ymd_hms(max(unobs_raw_acc_2023$UTC_datetime)))),
-tar_target(gps_2023, dplyr::select(ornitela_data_2023, tag_id, timestamp, dateOnly, ground_speed, location_lat, location_long, individual_id, tag_local_identifier)),
-tar_target(device_ids_2023, purrr::map(bouts_predictions_2023, ~.x$device_id[1]),
-           iteration = "list"),
-tar_target(focal_gps_2023, filter(gps_2023, tag_local_identifier == device_ids),
-           pattern = map(device_ids_2023),
-           iteration = "list"),
-tar_target(matches_2023, get_matches(bouts_predictions_2023, focal_gps_2023),
-           pattern = map(bouts_predictions_2023, focal_gps_2023),
-           iteration = "list"),
-tar_target(joined_2023, left_join(bouts_predictions_2023, matches,
-                                  by = c("device_id" = "tag_local_identifier",
-                                         "bout_id")),
-           pattern = map(bouts_predictions_2023, matches),
-           iteration = "list"),
-### Get feeding bouts
-tar_target(feeding_bouts_certain_2023, filter(joined_2023, pred == "Eating" &
-                                                !is.na(location_lat) &
-                                                .pred_Eating > 0.5) %>%
-             sf::st_as_sf(coords = c("location_long", "location_lat"),
-                          crs = "WGS84"),
-           pattern = map(joined_2023),
-           iteration = "list"),
-tar_target(feeding_bouts_station_2023, assign_fs(feeding_bouts_certain_2023, fs_union),
-           pattern = map(feeding_bouts_certain_2023),
-           iteration = "list")
+write_rds(feeding_bouts_station_2023, here("data/ACC/2023_hf_period/created/feeding_bouts_station_2023.RDS"))
+feeding_bouts_station_2023 <- readRDS(here("data/ACC/2023_hf_period/created/feeding_bouts_station_2023.RDS"))
