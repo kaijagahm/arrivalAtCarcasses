b1fb41dd88859711444ed9c1bc9764c8cbc9d85f
Kaija Gahm <kaija.gahm@gmail.com>
2024-09-27 14:57:46 -0700

    more tidying of stations and carcasses

--- _targets.R
+++ _targets.R
@@ -18,7 +18,7 @@ tar_option_set(
   controller = crew::crew_controller_local(workers = 10),
   format = "qs",
   error = "null",
-  packages = c("vultureUtils", "sf", "tidyverse", "move", "feather", "readxl", "elevatr", "here", "furrr", "future", "purrr", "igraph", "mapview", "parallel",   "ggplot2", "ggraph", "tidygraph", "moments", "tidymodels", "ranger", "parsnip", "caret", "zoo", "readxl", "data.table") # Packages that your targets need for their tasks.
+  packages = c("vultureUtils", "sf", "tidyverse", "move2", "feather", "readxl", "elevatr", "here", "furrr", "future", "purrr", "igraph", "mapview", "parallel",   "ggplot2", "ggraph", "tidygraph", "moments", "tidymodels", "ranger", "parsnip", "caret", "zoo", "readxl", "data.table", "readr") # Packages that your targets need for their tasks.
   
   # Pipelines that take a long time to run may benefit from
   # optional distributed computing. To use this capability
@@ -57,9 +57,14 @@ list(
   ### Get data
   tar_target(calibration_data, read_csv(here("ACC_algo_Marta_draft/Data/example_calibration.csv"))),
   tar_target(gv_model, readRDS(here("ACC_algo_Marta_draft/Data/gv_final_model_fit.rda"))),
-  tar_target(data_files_2024, list.files(here("data/ACC/2024_hf_period/raw/"), pattern = ".csv", full.names = T)),
+  tar_target(data_files_2024, list.files(here("data/ACC/2024_hf_period/raw/"), 
+                                         pattern = ".csv", full.names = T)),
+  tar_target(data_files_2023, list.files(here("data/ACC/2023_hf_period/raw/"), 
+                                         pattern = ".csv", full.names = T)),
   tar_target(unobs_raw_acc_2024, purrr::list_rbind(map(data_files_2024, ~as.data.frame(data.table::fread(.x, select = c("Latitude", "Longitude", "UTC_datetime", "UTC_date", "UTC_time", "datatype", "device_id", "acc_x", "acc_y", "acc_z")))))),
+  tar_target(unobs_raw_acc_2023, purrr::list_rbind(map(data_files_2023, ~as.data.frame(data.table::fread(.x, select = c("Latitude", "Longitude", "UTC_datetime", "UTC_date", "UTC_time", "datatype", "device_id", "acc_x", "acc_y", "acc_z")))))),
   tar_target(test, unobs_raw_acc_2024[1:10,]),
+  tar_target(test_23, unobs_raw_acc_2023[1:10,]), # XXX start here with 2023
   tar_target(a, split_data_fun_forloop(unobs_raw_acc_2024), iteration = "list"), # XXX this is called "a" because i couldn't get the damn pipeline to work. Just leave it for now.
   # tar_target(written_out, for(i in 1:length(split)){
   #   dev <- split[[i]]$device_id[1]  
@@ -119,7 +124,7 @@ list(
              pattern = map(bouts_predictions_2024, matches),
              iteration = "list"),
   ### Get feeding bouts
-  tar_target(feeding_bouts_certain, filter(joined, pred == "Eating" &
+  tar_target(feeding_bouts, filter(joined, pred == "Eating" &
                                              !is.na(location_lat) &
                                              .pred_Eating > 0.5) %>%
                sf::st_as_sf(coords = c("location_long", "location_lat"),
@@ -127,90 +132,17 @@ list(
              pattern = map(joined),
              iteration = "list"),
   ### Classify feeding station vs. not
-  tar_target(fs, readxl::read_excel(here("data/FeedingData from 2018_2024_Translated.xlsx")) %>%
-               rename("itmLong" = `ITM - LONG`,
-                      "itmLat" = `ITM - LAT`,
-                      "long" = `WGS84 - LONG`,
-                      "lat" = `WGS84 - LAT`,
-                      "date" = `Date Event`,
-                      "time" = `Event time`) %>%
-               select(long, lat, itmLong, itmLat, date, time) %>%
-               mutate(datetime = lubridate::ymd_hms(paste0(date, substr(time, 12, 19)))) %>%
-               distinct() %>%
-               st_as_sf(coords = c("long", "lat"), crs = "WGS84") %>%
-               st_transform(32636)),
-  tar_target(fs_buffered, sf::st_buffer(fs, dist = 100)),
-  tar_target(fs_union, sf::st_union(fs_buffered) %>% st_transform("WGS84")),
-  tar_target(feeding_bouts_station, assign_fs(feeding_bouts_certain, fs_union),
-             pattern = map(feeding_bouts_certain),
-             iteration = "list")#,
+  tar_target(stations, readRDS(here("data/created/stations.RDS"))),
+  tar_target(stations_buffered, sf::st_buffer(stations, dist = 100)),
+  tar_target(stations_union, sf::st_union(stations_buffered)),
+  tar_target(feeding_bouts_station, assign_fs(feeding_bouts, stations_union),
+             pattern = map(feeding_bouts),
+             iteration = "list"),
   
-  # ## 2023 period
-  # ### Get data
-  # tar_target(data_files_2023, list.files(here("data/ACC/2023_hf_period/raw/"), full.names = T)),
-  # tar_target(unobs_raw_acc_2023, data.table::fread(data_files_2023[1], select = c("Latitude", "Longitude", "UTC_datetime", "UTC_date", "UTC_time", "datatype", "device_id", "acc_x", "acc_y", "acc_z"))),
-  # tar_target(a_2023, split_data_fun_forloop(unobs_raw_acc_2023), iteration = "list"), # XXX this is called "a_2023" because i couldn't get the damn pipeline to work. Just leave it for now.
-  # tar_target(split_subset_2023, a_2023[6:9], iteration = "list"),
-  # ### Classify data
-  # tar_target(prepared_2023, prepare_dataset(a_2023, 
-  #                                           calibration = calibration_data),
-  #            pattern = map(a_2023), iteration = "list"),
-  # tar_target(bouts_2023, prepared_2023[,c("bout_id", "device_id", "start_int")] %>%
-  #              group_by(device_id, bout_id) %>%
-  #              summarize(start = min(start_int),
-  #                        end = max(start_int),
-  #                        .groups = "drop"),
-  #            pattern = map(prepared_2023),
-  #            iteration = "list"), 
-  # tar_target(predictions_2023, predict(gv_model, prepared_2023),
-  #            pattern = map(prepared_2023),
-  #            iteration = "list"),
-  # tar_target(scores_2023, predict(gv_model, prepared_2023, type = "prob"),
-  #            pattern = map(prepared_2023),
-  #            iteration = "list"),
-  # tar_target(bouts_predictions_2023, 
-  #            distinct(get_bouts_predictions(prepared_2023,
-  #                                           predictions_2023,
-  #                                           scores_2023,
-  #                                           bouts_2023)),
-  #            pattern = map(prepared_2023, 
-  #                          predictions_2023, 
-  #                          scores_2023, 
-  #                          bouts_2023),
-  #            iteration = "list"),
-  # tar_target(ornitela_data_2023,
-  #            vultureUtils::downloadVultures(loginObject = loginObject,
-  #                                           removeDup = T, dfConvert = T,
-  #                                           quiet = T,
-  #                                           dateTimeStartUTC =
-  #                                             lubridate::ymd_hms(min(unobs_raw_acc_2023$UTC_datetime)),
-  #                                           dateTimeEndUTC =
-  #                                             lubridate::ymd_hms(max(unobs_raw_acc_2023$UTC_datetime)))),
-  # tar_target(gps_2023, dplyr::select(ornitela_data_2023, tag_id, timestamp, dateOnly, ground_speed, location_lat, location_long, individual_id, tag_local_identifier)),
-  # tar_target(device_ids_2023, purrr::map(bouts_predictions_2023, ~.x$device_id[1]),
-  #            iteration = "list"),
-  # tar_target(focal_gps_2023, filter(gps_2023, tag_local_identifier == device_ids),
-  #            pattern = map(device_ids_2023),
-  #            iteration = "list"),
-  # tar_target(matches_2023, get_matches(bouts_predictions_2023, focal_gps_2023),
-  #            pattern = map(bouts_predictions_2023, focal_gps_2023),
-  #            iteration = "list"),
-  # tar_target(joined_2023, left_join(bouts_predictions_2023, matches,
-  #                                   by = c("device_id" = "tag_local_identifier",
-  #                                          "bout_id")),
-  #            pattern = map(bouts_predictions_2023, matches),
-  #            iteration = "list"),
-  # ### Get feeding bouts
-  # tar_target(feeding_bouts_certain_2023, filter(joined_2023, pred == "Eating" &
-  #                                                 !is.na(location_lat) &
-  #                                                 .pred_Eating > 0.5) %>%
-  #              sf::st_as_sf(coords = c("location_long", "location_lat"),
-  #                           crs = "WGS84"),
-  #            pattern = map(joined_2023),
-  #            iteration = "list"),
-  # tar_target(feeding_bouts_station_2023, assign_fs(feeding_bouts_certain_2023, fs_union),
-  #            pattern = map(feeding_bouts_certain_2023),
-  #            iteration = "list")
+  # in case we wanted to assign station vs. non-station based on carcass placement instead of station coordinates
+  tar_target(carcasses_inpa, readRDS(here("data/created/carcasses_inpa.RDS"))),
+  tar_target(carcasses_buffered, sf::st_buffer(carcasses_inpa, dist = 100)),
+  tar_target(carcasses_union, sf::st_union(carcasses_buffered))
 )
 
 
--- carcass_data_translation.R
+++ carcass_data_translation.R
@@ -70,4 +70,48 @@ carcasses_inpa <- bind_rows(old, new)
 carcasses_inpa <- carcasses_inpa %>%
   mutate(datetime = lubridate::ymd_hms(paste0(as.character(lubridate::ymd(date)), substr(time, 12, 19))))
 
-write_csv(carcasses_inpa, here("data/created/carcasses_inpa.csv"))
+sort(unique(carcasses_inpa$stationName))
+
+carcasses_inpa <- carcasses_inpa %>%
+  mutate(cage = case_when(str_detect(stationName, "cage") ~ T,
+                          .default = F))
+
+carcasses_inpa_sf <- st_as_sf(carcasses_inpa, coords = c("long", "lat"), crs = "WGS84", remove = F) %>%
+  st_transform(32636)
+
+write_rds(carcasses_inpa, here("data/created/carcasses_inpa.RDS"))
+
+# Now the feeding stations ------------------------------------------------
+north <- readxl::read_excel(here("data/raw/feeding_stations_north.xlsx"), sheet = 2) %>%
+  setNames(c("stationName", "region", "itmLong", "itmLat", "lat", "long"))
+south <- readxl::read_excel(here("data/raw/feeding_station_south_coordinates.xlsx")) %>%
+  setNames(c("stationName", "active", "itmLong", "itmLat", "lat", "long"))
+
+stations <- bind_rows(north, south) 
+stations$lat <- str_remove_all(stations$lat, "\\s")
+stations$long <- str_remove_all(stations$long, "\\s")
+stations <- stations %>%
+  mutate(across(c("long", "lat"), as.numeric))
+
+stations <- stations %>%
+  st_as_sf(coords = c("long", "lat"), remove = F, crs = "WGS84") %>%
+  st_transform(32636)
+
+write_rds(stations, here("data/created/stations.RDS"))
+
+fs <- sort(unique(stations$stationName))
+
+mapview(carcasses_inpa_sf, label = "stationName", color = "blue", col.regions = "blue") +
+  mapview(stations, label = "stationName", color = "red", col.regions = "red")
+
+# Looking at this map shows us that 1) not all INPA carcasses are deposited at feeding stations (what's up with that? is this not a complete list of feeding stations?) # XXX add this to the report
+
+# Correspondence in names based on overlapping dots
+# 1. Ben_Yair_view = observation ben-Yair
+# 2. Gorni_hill = Gorni hill
+# 3. North_Golan = north golan
+# 4. Hai_Bar_Carmel = Carmel - cage or Hai-bar feeding station
+# Hever doesn't seem to be included on the total list of feeding stations
+# 5. Amiaz = Other (one of the "Other"s)
+# this is going to take forever--I need to show this to them in the report.
+
