89a0a858c9bd090427afe92ee4a70fb5ba6dcd52
Kaija Gahm <kaija.gahm@gmail.com>
2024-09-27 15:09:30 -0700

    commented out mining data so i can re-run w 2024 hf-acc data

--- _targets.R
+++ _targets.R
@@ -36,21 +36,21 @@ lapply(list.files("R", full.names = TRUE), source) # source all scripts in the R
 
 list(
   # Prepare data (mining period, geofenced)
-  tar_target(geofences, get_geofences()),
-  tar_target(tag_sns_file, "data/geofence_tags_6Aug23_45.xlsx", format = "file"),
-  tar_target(tag_sns, get_sns(tag_sns_file)),
+  # tar_target(geofences, get_geofences()),
+  # tar_target(tag_sns_file, "data/geofence_tags_6Aug23_45.xlsx", format = "file"),
+  # tar_target(tag_sns, get_sns(tag_sns_file)),
   tar_target(pw, "movebankCredentials/pw.Rda", format = "file"),
   tar_target(loginObject, get_loginObject(pw)),
   tar_target(ornitela, get_ornitela(loginObject)),
   tar_target(ww_file, "data/whoswho_vultures_20230920_new.xlsx", format = "file"),
-  tar_target(fixed_names, fix_names(ornitela, ww_file)),
-  tar_target(removed_periods, remove_periods(ww_file, fixed_names)),
-  tar_target(cleaned, clean_data(removed_periods)),
-  tar_target(capture_sites, "data/capture_sites.csv", format = "file"),
-  tar_target(carmel, "data/all_captures_carmel_2010-2021.csv", format = "file"),
-  tar_target(removed_captures, remove_captures(capture_sites, carmel, cleaned)),
-  tar_target(with_age_sex, attach_age_sex(removed_captures, ww_file)),
-  tar_target(hires_tags, get_hires_tags(with_age_sex, tag_sns)),
+  # tar_target(fixed_names, fix_names(ornitela, ww_file)),
+  # tar_target(removed_periods, remove_periods(ww_file, fixed_names)),
+  # tar_target(cleaned, clean_data(removed_periods)),
+  # tar_target(capture_sites, "data/capture_sites.csv", format = "file"),
+  # tar_target(carmel, "data/all_captures_carmel_2010-2021.csv", format = "file"),
+  # tar_target(removed_captures, remove_captures(capture_sites, carmel, cleaned)),
+  # tar_target(with_age_sex, attach_age_sex(removed_captures,t ww_file)),
+  # tar_target(hires_tags, get_hires_tags(with_age_sex, tag_sns)),
   
   # High-frequency ACC
   ## 2024 period
@@ -64,85 +64,85 @@ list(
   tar_target(unobs_raw_acc_2024, purrr::list_rbind(map(data_files_2024, ~as.data.frame(data.table::fread(.x, select = c("Latitude", "Longitude", "UTC_datetime", "UTC_date", "UTC_time", "datatype", "device_id", "acc_x", "acc_y", "acc_z")))))),
   tar_target(unobs_raw_acc_2023, purrr::list_rbind(map(data_files_2023, ~as.data.frame(data.table::fread(.x, select = c("Latitude", "Longitude", "UTC_datetime", "UTC_date", "UTC_time", "datatype", "device_id", "acc_x", "acc_y", "acc_z")))))),
   tar_target(test, unobs_raw_acc_2024[1:10,]),
-  tar_target(test_23, unobs_raw_acc_2023[1:10,]), # XXX start here with 2023
-  tar_target(a, split_data_fun_forloop(unobs_raw_acc_2024), iteration = "list"), # XXX this is called "a" because i couldn't get the damn pipeline to work. Just leave it for now.
-  # tar_target(written_out, for(i in 1:length(split)){
-  #   dev <- split[[i]]$device_id[1]  
-  #   filename <- paste0("/", dev, ".csv")
-  #   data.table::fwrite(split[[i]], 
-  #                      file = paste0(here("data/ACC/2024_hf_period/created/devices"), 
-  #                                    filename))
-  # }),
-  # tar_target(split_files_2024, list.files(here("data/ACC/2024_hf_period/created/devices/"), pattern = ".csv", full.names = T)),
-  tar_target(split_subset, a[6:9], iteration = "list"),
-  ### Classify data
-  tar_target(prepared_2024, prepare_dataset(a, calibration = calibration_data),
-             pattern = map(a), iteration = "list"),
-  tar_target(bouts_2024, prepared_2024[,c("bout_id", "device_id", "start_int")] %>%
-               group_by(device_id, bout_id) %>%
-               summarize(start = min(start_int),
-                         end = max(start_int),
-                         .groups = "drop"),
-             pattern = map(prepared_2024),
-             iteration = "list"), 
-  tar_target(predictions_2024, predict(gv_model, prepared_2024),
-             pattern = map(prepared_2024),
-             iteration = "list"),
-  tar_target(scores_2024, predict(gv_model, prepared_2024, type = "prob"),
-             pattern = map(prepared_2024),
-             iteration = "list"),
-  tar_target(bouts_predictions_2024, 
-             distinct(get_bouts_predictions(prepared_2024,
-                                   predictions_2024,
-                                   scores_2024,
-                                   bouts_2024)),
-             pattern = map(prepared_2024, 
-                           predictions_2024, 
-                           scores_2024, 
-                           bouts_2024),
-             iteration = "list"),
-  tar_target(ornitela_data_2024,
-             vultureUtils::downloadVultures(loginObject = loginObject,
-                                            removeDup = T, dfConvert = T,
-                                            quiet = T,
-                                            dateTimeStartUTC =
-                                              lubridate::ymd_hms(min(unobs_raw_acc_2024$UTC_datetime)),
-                                            dateTimeEndUTC =
-                                              lubridate::ymd_hms(max(unobs_raw_acc_2024$UTC_datetime)))),
-  tar_target(gps, dplyr::select(ornitela_data_2024, tag_id, timestamp, dateOnly, ground_speed, location_lat, location_long, individual_id, tag_local_identifier)),
-  tar_target(device_ids, purrr::map(bouts_predictions_2024, ~.x$device_id[1]),
-             iteration = "list"),
-  tar_target(focal_gps, filter(gps, tag_local_identifier == device_ids),
-             pattern = map(device_ids),
-             iteration = "list"),
-  tar_target(matches, get_matches(bouts_predictions_2024, focal_gps),
-             pattern = map(bouts_predictions_2024, focal_gps),
-             iteration = "list"),
-  tar_target(joined, left_join(bouts_predictions_2024, matches,
-                               by = c("device_id" = "tag_local_identifier",
-                                      "bout_id")),
-             pattern = map(bouts_predictions_2024, matches),
-             iteration = "list"),
-  ### Get feeding bouts
-  tar_target(feeding_bouts, filter(joined, pred == "Eating" &
-                                             !is.na(location_lat) &
-                                             .pred_Eating > 0.5) %>%
-               sf::st_as_sf(coords = c("location_long", "location_lat"),
-                            crs = "WGS84"),
-             pattern = map(joined),
-             iteration = "list"),
-  ### Classify feeding station vs. not
-  tar_target(stations, readRDS(here("data/created/stations.RDS"))),
-  tar_target(stations_buffered, sf::st_buffer(stations, dist = 100)),
-  tar_target(stations_union, sf::st_union(stations_buffered)),
-  tar_target(feeding_bouts_station, assign_fs(feeding_bouts, stations_union),
-             pattern = map(feeding_bouts),
-             iteration = "list"),
-  
-  # in case we wanted to assign station vs. non-station based on carcass placement instead of station coordinates
-  tar_target(carcasses_inpa, readRDS(here("data/created/carcasses_inpa.RDS"))),
-  tar_target(carcasses_buffered, sf::st_buffer(carcasses_inpa, dist = 100)),
-  tar_target(carcasses_union, sf::st_union(carcasses_buffered))
+  tar_target(test_23, unobs_raw_acc_2023[1:10,])#, # XXX start here with 2023
+  # tar_target(a, split_data_fun_forloop(unobs_raw_acc_2024), iteration = "list"), # XXX this is called "a" because i couldn't get the damn pipeline to work. Just leave it for now.
+  # # tar_target(written_out, for(i in 1:length(split)){
+  # #   dev <- split[[i]]$device_id[1]  
+  # #   filename <- paste0("/", dev, ".csv")
+  # #   data.table::fwrite(split[[i]], 
+  # #                      file = paste0(here("data/ACC/2024_hf_period/created/devices"), 
+  # #                                    filename))
+  # # }),
+  # # tar_target(split_files_2024, list.files(here("data/ACC/2024_hf_period/created/devices/"), pattern = ".csv", full.names = T)),
+  # tar_target(split_subset, a[6:9], iteration = "list"),
+  # ### Classify data
+  # tar_target(prepared_2024, prepare_dataset(a, calibration = calibration_data),
+  #            pattern = map(a), iteration = "list"),
+  # tar_target(bouts_2024, prepared_2024[,c("bout_id", "device_id", "start_int")] %>%
+  #              group_by(device_id, bout_id) %>%
+  #              summarize(start = min(start_int),
+  #                        end = max(start_int),
+  #                        .groups = "drop"),
+  #            pattern = map(prepared_2024),
+  #            iteration = "list"), 
+  # tar_target(predictions_2024, predict(gv_model, prepared_2024),
+  #            pattern = map(prepared_2024),
+  #            iteration = "list"),
+  # tar_target(scores_2024, predict(gv_model, prepared_2024, type = "prob"),
+  #            pattern = map(prepared_2024),
+  #            iteration = "list"),
+  # tar_target(bouts_predictions_2024, 
+  #            distinct(get_bouts_predictions(prepared_2024,
+  #                                  predictions_2024,
+  #                                  scores_2024,
+  #                                  bouts_2024)),
+  #            pattern = map(prepared_2024, 
+  #                          predictions_2024, 
+  #                          scores_2024, 
+  #                          bouts_2024),
+  #            iteration = "list"),
+  # tar_target(ornitela_data_2024,
+  #            vultureUtils::downloadVultures(loginObject = loginObject,
+  #                                           removeDup = T, dfConvert = T,
+  #                                           quiet = T,
+  #                                           dateTimeStartUTC =
+  #                                             lubridate::ymd_hms(min(unobs_raw_acc_2024$UTC_datetime)),
+  #                                           dateTimeEndUTC =
+  #                                             lubridate::ymd_hms(max(unobs_raw_acc_2024$UTC_datetime)))),
+  # tar_target(gps, dplyr::select(ornitela_data_2024, tag_id, timestamp, dateOnly, ground_speed, location_lat, location_long, individual_id, tag_local_identifier)),
+  # tar_target(device_ids, purrr::map(bouts_predictions_2024, ~.x$device_id[1]),
+  #            iteration = "list"),
+  # tar_target(focal_gps, filter(gps, tag_local_identifier == device_ids),
+  #            pattern = map(device_ids),
+  #            iteration = "list"),
+  # tar_target(matches, get_matches(bouts_predictions_2024, focal_gps),
+  #            pattern = map(bouts_predictions_2024, focal_gps),
+  #            iteration = "list"),
+  # tar_target(joined, left_join(bouts_predictions_2024, matches,
+  #                              by = c("device_id" = "tag_local_identifier",
+  #                                     "bout_id")),
+  #            pattern = map(bouts_predictions_2024, matches),
+  #            iteration = "list"),
+  # ### Get feeding bouts
+  # tar_target(feeding_bouts, filter(joined, pred == "Eating" &
+  #                                            !is.na(location_lat) &
+  #                                            .pred_Eating > 0.5) %>%
+  #              sf::st_as_sf(coords = c("location_long", "location_lat"),
+  #                           crs = "WGS84"),
+  #            pattern = map(joined),
+  #            iteration = "list"),
+  # ### Classify feeding station vs. not
+  # tar_target(stations, readRDS(here("data/created/stations.RDS"))),
+  # tar_target(stations_buffered, sf::st_buffer(stations, dist = 100)),
+  # tar_target(stations_union, sf::st_union(stations_buffered)),
+  # tar_target(feeding_bouts_station, assign_fs(feeding_bouts, stations_union),
+  #            pattern = map(feeding_bouts),
+  #            iteration = "list"),
+  # 
+  # # in case we wanted to assign station vs. non-station based on carcass placement instead of station coordinates
+  # tar_target(carcasses_inpa, readRDS(here("data/created/carcasses_inpa.RDS"))),
+  # tar_target(carcasses_buffered, sf::st_buffer(carcasses_inpa, dist = 100)),
+  # tar_target(carcasses_union, sf::st_union(carcasses_buffered))
 )
 
 
