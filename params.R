0efb51d671a8f2e0bcd7a4c22b9686642a13b799
Kaija Gahm <kaija.gahm@gmail.com>
2024-10-28 16:18:24 -0700

    arrival plots

---
+++ params.R
@@ -0,0 +1,17 @@
+# Parameters
+
+# general
+flight_speed <- 5
+
+# arrivals_plots.R
+ncores <- 10
+
+# hf-acc_carcass_arrivals.R
+dist_gps_consider <- 1000
+dist_viz_flying <- 1000
+dist_near_flying <- 500
+dist_far_ground <- 1000
+dist_near_ground <- 300
+dist_at_ground <- 100
+carcass_dur_days <- 5
+
