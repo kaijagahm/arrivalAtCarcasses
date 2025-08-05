5ef95497270f37177213adc8409e13ec70232b9f
Kaija Gahm <kaija.gahm@gmail.com>
2025-08-05 14:42:29 -0700

    working on a more robust way to download the data using move2

--- scripts/download_gps_data.R
+++ scripts/download_gps_data.R
@@ -3,17 +3,44 @@ library(move2)
 library(targets)
 library(readr)
 library(here)
-tar_load(minmax_dates)
+tar_load(minmax_buff)
 # don't forget to interactively authenticate!
+
+test_orn_22 <- move2::movebank_download_study(1252551761,
+                                              sensor_type_id = "gps",
+                                              timestamp_start = minmax_buff[[1]] + days(25),
+                                              timestamp_end = minmax_buff[[2]] - days(25))
+out <- left_join(test_orn_22, mt_track_data(test_orn_22))
+
+test_inp_22 <- move2::movebank_download_study(6071688,
+                                              sensor_type_id = "gps",
+                                              timestamp_start = minmax_buff[[1]] + days(25),
+                                              timestamp_end = minmax_buff[[2]] - days(25))
+
+test_orn_24 <- move2::movebank_download_study(1252551761,
+                                              sensor_type_id = "gps",
+                                              timestamp_start = minmax_buff[[5]] + days(25),
+                                              timestamp_end = minmax_buff[[6]] - days(25))
+
+test_inp_24 <- move2::movebank_download_study(6071688,
+                                              sensor_type_id = "gps",
+                                              timestamp_start = minmax_buff[[5]] + days(25),
+                                              timestamp_end = minmax_buff[[6]] - days(25))
+
+dim(test_orn_22)
+dim(test_inp_22)
+dim(test_orn_24)
+dim(test_inp_24)
+
+mt_track_id_column(test_orn_22)
+mt_track_id_column(test_inp_22)
+mt_track_id_column(test_orn_24)
+mt_track_id_column(test_inp_24)
 
 ornitela_data_2022 <- move2::movebank_download_study(1252551761,
                                           sensor_type_id = "gps",
-                                          timestamp_start = minmax_dates[[1]],
-                                          timestamp_end = minmax_dates[[2]])
-test <- move2::movebank_download_study(1252551761,
-                                                     sensor_type_id = "gps",
-                                                     timestamp_start = minmax_dates[[1]],
-                                                     timestamp_end = minmax_dates[[4]])
+                                          timestamp_start = minmax_buff[[1]],
+                                          timestamp_end = minmax_buff[[2]])
 ornitela_data_2023 <- move2::movebank_download_study(1252551761,
                                           sensor_type_id = "gps",
                                           timestamp_start = minmax_dates[[3]],
