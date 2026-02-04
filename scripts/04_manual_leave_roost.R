76520eeb6b1b76df223eeb381c6966d24e9285b6
Kaija Gahm <kaija.gahm@gmail.com>
2026-02-04 14:22:38 -0800

    lots of progress on manual calculation of dyad departures and trajectories.

--- scripts/04_manual_leave_roost.R
+++ scripts/04_manual_leave_roost.R
@@ -11,6 +11,8 @@ library(tidyverse)
 library(sf)
 library(targets)
 library(mapview)
+library(ggraph)
+library(tidygraph)
 
 # get roost location data
 tar_load(roosts_stn) # this isn't great for our purposes because it's on a per-carcass basis, but that's ok
@@ -82,10 +84,112 @@ get_leftroost <- function(ordered_df, threshold){
 
 leftpoints <- map_dbl(data_timeordered, ~get_leftroost(.x, threshold = 2))
 
-test <- map2(data_timeordered, leftpoints, ~{
+data_timeordered <- map2(data_timeordered, leftpoints, ~{
   .x$left_roost <- FALSE
   if(!is.na(.y)){
     .x$left_roost[.y] <- TRUE
   }
   return(.x)
 })
+
+data_rejoined <- purrr::list_rbind(data_timeordered)
+
+leaving_points <- data_rejoined %>%
+  filter(left_roost)
+
+data_rejoined %>%
+  filter(date_il == min(date_il)) %>%
+  ggplot(aes(x = timestamp_il, y = individual_local_identifier, color = in_a_roost, size = left_roost))+
+  geom_point()+
+  theme_minimal()+
+  scale_size_manual(values = c(0.5, 1.5), name = "Departure")+
+  theme(axis.title.x = element_blank())
+
+# Now apply to each date
+leaving_points_dates <- leaving_points %>% group_by(date_il) %>%
+  group_split()
+
+# Make a "same roost" matrix
+# RoostID represents the roost that they left from, one point before this
+roost_mats <- purrr::map(leaving_points_dates, ~{
+  mat <- outer(.x$roostID, .x$roostID, FUN = "==") * 1
+  rownames(mat) <- .x$individual_local_identifier
+  colnames(mat) <- .x$individual_local_identifier
+  return(mat)
+  })
+
+roost_mats_long <- purrr::map(roost_mats, ~{
+  .x %>% as.data.frame() %>% rownames_to_column("ID1") %>%
+    pivot_longer(cols = -ID1, names_to = "ID2", values_to = "same_roost")
+})
+
+# Make a "time difference" matrix
+difftime_mats <- purrr::map(leaving_points_dates, ~{
+  mat <- outer(.x$timestamp_il, .x$timestamp_il,
+               function(t1, t2) as.numeric(abs(difftime(t1, t2, units = "mins"))))
+  rownames(mat) <- .x$individual_local_identifier
+  colnames(mat) <- .x$individual_local_identifier
+  return(mat)
+  })
+
+difftime_mats_long <- purrr::map(difftime_mats, ~{
+  .x %>% as.data.frame() %>% rownames_to_column("ID1") %>%
+    pivot_longer(cols = -ID1, names_to = "ID2", values_to = "time_diff_min")
+})
+
+# Find cases where roost_mat == 1 and difftime_mat < 10 mins
+both <- purrr::map2(roost_mats_long, difftime_mats_long, left_join)
+sync_departures <- purrr::map(both, ~{
+  .x %>% filter(same_roost == 1) %>%
+    select(-same_roost) %>%
+    filter(ID1 < ID2) # remove self loops and repeats
+  })
+
+# Now let's visualize these as roost departure networks. Is the 10 min threshold too high? Do we have dyads, or a lot of groups leaving together?
+edgelists <- map2(sync_departures, leaving_points_dates, ~{
+  .x %>% rename("from" = ID1, "to" = ID2) %>%
+    mutate(weight = 1 / (time_diff_min + 1))
+})
+
+departure_nets <- map2(edgelists, leaving_points_dates, ~{
+  tbl_graph(edges = .x, directed = F) %>%
+    activate(nodes) %>%
+    left_join(distinct(select(.y, individual_local_identifier, roostID)), by = c("name" = "individual_local_identifier"))
+  })
+
+departure_dates <- map_chr(leaving_points_dates, ~as.character(.x$date_il[1]))
+
+departure_graphs <- map2(departure_nets, departure_dates, ~{
+  .x %>% activate(edges) %>%
+    filter(time_diff_min <= 10) %>%
+    ggraph(layout = "fr") +
+    geom_node_text(aes(label = name), repel = TRUE, size = 3) +
+    geom_edge_link(aes(width = weight), alpha = 0.7, 
+                   show.legend = F) +
+    geom_node_point(size = 2, aes(color = factor(roostID)),
+                    show.legend = F) +
+    theme_graph()+
+    scale_color_discrete(name = "Roost polygon")+
+    scale_edge_width(name = "Temporal proximity", range = c(0.1, 2.5))+
+    facet_nodes(~factor(roostID), nrow = 3) +
+    NULL+
+    labs(title = paste0("Synchronized roost departures,\n", .y),
+         subtitle = "10 min threshold",
+         caption = "Heavier edges represent more similar departure times (inverse time interval).\n On this scale, 1 = 0 min difference; 0 = 10 min difference.\n Time differences > 10 min are not shown.")
+})
+
+departure_graphs[[1]]
+departure_graphs[[2]]
+departure_graphs[[3]]
+
+# What about the trajectories after the departures?
+head(test_gps)
+
+# Get data for a single day, and perhaps for a single roost
+# Plot lines onto a map to show trajectories
+# How do we calculate distances between the trajectories to demonstrate co-movement? We can't necessarily assume that points line up with each other.
+# I'm sure someone has thought of this--maybe we can make the points into continuous trajectories using the move2 or ctmm package?
+# "We tested the ‘following behaviour’ for the subset of dyads that departed synchronously from the roost and performed long daily flights (daily displacement more than 15 km; 518 dyads; see the electronic supplementary material,"
+# "We focused on three indices of the ‘following behaviour’, (i) the proportion of the flight time that individuals spent close to each other (within detection range); (ii) the mean distance between individuals during the flight; (iii) whether the informed was leading the dyad, namely closer to the goal site and how this changed along the joint flight."
+
+# So we need to calculate individual daily displacement, restrict the GPS data to only dyads that flew far enough (does this restriction make sense for us, or not?) and then plot it onto the map...
