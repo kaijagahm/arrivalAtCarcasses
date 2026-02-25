cea6c0284ad2a035eac792e302b21cd6f8506de5
Kaija Gahm <kaija.gahm@gmail.com>
2026-02-25 09:26:00 -0800

    ran some tests of STbayes with different parameters

--- 00_test_STbayes_owndata_dynamic.R
+++ 00_test_STbayes_owndata_dynamic.R
@@ -29,66 +29,19 @@ tar_load(dds)
 stb_mins # 30 mins
 seeds <- character(0)
 time_window <- stb_mins / 60
-seeds <- gps %>%
-  filter(time_since_carcass >= -time_window,
-         time_since_carcass <= 0,
-         (ground_speed > gps_spd & dist_to_carcass <= ddf) |
-           (ground_speed <= gps_spd & dist_to_carcass <= dds)) %>%
-  distinct(individual_local_identifier) %>%
-  pull(individual_local_identifier) %>%
-  as.character()
+
+seeds <- get_seeds(gps, ddf, dds, gps_spd)
 seeds # these are the names of the seed individuals
 
 # Get first sightings
-first_sightings <- gps %>%
-  filter(time_since_carcass >= 0 & time_since_carcass <= sighting_time_max_hours) %>%
-  filter((ground_speed > gps_spd & dist_to_carcass <= ddf) |
-           (ground_speed <= gps_spd & dist_to_carcass <= dds)) %>%
-  group_by(individual_local_identifier) %>%
-  arrange(time_since_carcass, timestamp) %>%
-  slice(1) %>%
-  ungroup() %>%
-  arrange(time_since_carcass)
-
-first_sightings <- first_sightings %>%
-  filter(!(individual_local_identifier %in% seeds))
+first_sightings <- get_first_sightings(gps, sighting_time_max_hours, gps_spd, ddf, dds, seeds)
 
 all_indivs_sorted <- sort(unique(as.character(gps$individual_local_identifier)))
 
 # Now we have the event data; time to format it the way that STbayes needs.
-
-test_event_data <- first_sightings %>%
-  st_drop_geometry() %>%
-  dplyr::mutate(trial = carc_id, t_end = max(timestamp_il)) %>%
-  dplyr::select(individual_local_identifier, trial, time_since_carcass, t_end) %>%
-  mutate(time = as.numeric(time_since_carcass, units = "secs")) %>%
-  rename("id" = individual_local_identifier) %>%
-  mutate(t_end = max(time)) %>%
-  select(id, trial, time, t_end)
-
-#time: Integer or float values indicating the time (TADA) or order (OADA) in which the individual was recorded as first informed/knowledgable. If an individual had the event occur prior to the start of the observation period (e.g. pre-trained demonstrator), set as 0. These left censored individuals will not contribute to the likelihood calculation. 
-# add the seeds back in (pre-trained demonstrators)
-test_event_data <- bind_rows(data.frame(id = seeds, trial = carc_id, t_end = max(test_event_data$time), time = 0),
-                             test_event_data)
-
-#If an individual never learned during the observation period, set its value tend+1. These will be treated as right-censored individuals in the likelihood calculation.
-# okay so for these, we need to figure out if there are any individuals in the gps dataset that don't appear in the first sightings.
-never_learned <- all_indivs_sorted[!(all_indivs_sorted %in% test_event_data$id)]
-t_end <- test_event_data$t_end[1]
-test_event_data <- bind_rows(test_event_data,
-                             data.frame(id = never_learned, trial = carc_id, t_end = t_end, time = t_end + 1)) %>%
-  arrange(time, id) %>%
-  mutate(across(c(time, t_end), as.integer)) # should be INTEGER, not NUMERIC, so the code will work properly
-
-# Okay great, that wasn't so bad! Now we have our test event data.
-# Now we need our edge_list
-
-#"This edge list gives the network connections of each individual in a long format. You can give a sparse edge list that doesn’t include all dyads, but importantly, all individuals must be accounted for in the edge list. The first three columns must be:
-#trial: Character or numeric column indicating which trial the networks belong to.
-#focal: Character or numeric column of individual identities.
-#other: Character or numeric column of individual identities."
-
+event_data <- format_event_data(first_sightings, seeds, all_indivs_sorted)
 tar_load(hours_after_carcass)
+
 gps_fornetwork <- gps %>%
   filter(timestamp_il %in% carc$date:(carc$date + lubridate::hours(hours_after_carcass))) %>%
   mutate(time = as.numeric(time_since_carcass, units = "secs")) %>% # this will now correspond to the numeric times in test_event_data.
@@ -98,16 +51,39 @@ gps_fornetwork <- gps %>%
 # Note: it does NOT say what to do if there are seed individuals... I assume I don't provide a network for those, since they're set to 0, so the first one will just be from 0 through te1? (e1 = event 1)
 # Also, what they're saying about the end of the observation period seems to contradict how they said to encode the censored individuals (i.e. set them to tend+1)
 
-cutpoints <- unique(test_event_data$time)
+cutpoints <- unique(event_data$time)
 sort(unique(cut(gps_fornetwork$time, breaks = cutpoints))) # these look right!
 gps_fornetwork$network <- cut(gps_fornetwork$time, breaks = cutpoints)
 length(unique(gps_fornetwork$network))
 network_intervals <- sort(unique(gps_fornetwork$network))
 
 gps_list <- gps_fornetwork %>% arrange(network) %>% group_split(network, .keep = TRUE)
+# now we need to split cumulative by day
+# test <- event_data %>% left_join(select(first_sightings, individual_local_identifier, date_il), by = c("id" = "individual_local_identifier"))
+# test$network <- cut(test$time, breaks = cutpoints)
+# gps_fornetwork <- left_join(gps_fornetwork, select(st_drop_geometry(test), date_il, network), by = "network")
+gps_list_cumulative <- gps_fornetwork %>%
+  arrange(date_il, network) %>%
+  group_split(date_il) %>%
+  map(function(day_df) {
+    
+    # get unique group_idx in the order they appear within the day
+    groups <- unique(day_df$network)
+    
+    map(seq_along(groups), function(i) {
+      day_df %>%
+        filter(network %in% groups[1:i])
+    })
+    
+  }) %>%
+  flatten()
+
 tar_load(rp)
 dynamic_networks <- map(gps_list, ~get_fl_weighted(dat = .x, dist = ddf, rp = rp, spd = gps_spd))
 dynamic_networks_fixed <- fix_nets(nets = dynamic_networks, indivs = all_indivs_sorted)
+
+dynamic_networks_cumul <- map(gps_list_cumulative, ~get_fl_weighted(dat = .x, dist = ddf, rp = rp, spd = gps_spd))
+dynamic_networks_cumul_fixed <- fix_nets(nets = dynamic_networks_cumul, indivs = all_indivs_sorted)
 
 networks_long <- map(dynamic_networks_fixed, ~{
   out <- .x %>% rownames_to_column(var = "focal") %>%
@@ -117,36 +93,62 @@ networks_long <- map(dynamic_networks_fixed, ~{
 })
 networks_long_dynamic <- purrr::list_rbind(networks_long, names_to = "time")
 
+networks_long_cumul <- map(dynamic_networks_cumul_fixed, ~{
+  out <- .x %>% rownames_to_column(var = "focal") %>%
+    pivot_longer(cols = -focal, names_to = "other", values_to = "flight_sri") %>%
+    mutate(trial = carc_id)
+  return(out)
+})
+networks_long_dynamic_cumul <- purrr::list_rbind(networks_long_cumul, names_to = "time")
+
 # Network must contain all individuals
 # "The networks dataframe is used as the reference for all unique IDs, thus each ID must be included at least once in either the focal or other column. If a dyad is absent, their connection is assumed to be zero."
 all(sort(unique(c(networks_long_dynamic$focal, networks_long_dynamic$other))) == all_indivs_sorted) #TRUE
+all(sort(unique(c(networks_long_dynamic_cumul$focal, networks_long_dynamic_cumul$other))) == all_indivs_sorted) #TRUE
 
-data_list <- import_user_STb(event_data = test_event_data, 
+data_list <- import_user_STb(event_data = event_data, 
                              networks = networks_long_dynamic,
                              network_type = "undirected") # this provides really helpful confirmatory checks
+data_list_cumul <- import_user_STb(event_data = event_data, 
+                             networks = networks_long_dynamic_cumul,
+                             network_type = "undirected")
 
 # "If you were making a multi-network model, you could add as many columns as you want."
 # good to know for later!
 
 model_full_dynamic <- generate_STb_model(data_list, gq = T, est_acqTime = T)
-cat(model_full_dynamic) # really long string with the model code
+model_full_dynamic_cumul <- generate_STb_model(data_list_cumul, gq = T, est_acqTime = T)
 
-# full_fit_dynamic <- fit_STb(data_list,
-#                     model_full_dynamic,
-#                     parallel_chains = 3,
-#                     chains = 3,
-#                     cores = 3,
-#                     iter = 1000,
-#                     refresh=100
-# ) # okay so this takes forever, like FOREVER oh my god
-# STb_save(full_fit_dynamic, output_dir = "data/cmdstan_saves", name="dynamic")
-full_fit <- readRDS('data/cmdstan_saves/dynamic.rds') 
-STb_summary(full_fit, digits = 3)
+fit_dynamic <- fit_STb(data_list,
+                    model_full_dynamic,
+                    parallel_chains = 3,
+                    chains = 3,
+                    cores = 3,
+                    iter = 500,
+                    refresh=50)
+STb_save(fit_dynamic, output_dir = "data/cmdstan_saves", name="dynamic")
+fit_dynamic <- readRDS('data/cmdstan_saves/dynamic.rds') 
+
+fit_dynamic_cumul <- fit_STb(data_list_cumul,
+                       model_full_dynamic_cumul,
+                       parallel_chains = 3,
+                       chains = 3,
+                       cores = 3,
+                       iter = 500,
+                       refresh=50)
+
+STb_save(fit_dynamic_cumul, output_dir = "data/cmdstan_saves", name="dynamic_cumul")
+fit_dynamic_cumul <- readRDS('data/cmdstan_saves/dynamic_cumul.rds') 
+
+STb_summary(fit_dynamic, digits = 3)
+STb_summary(fit_dynamic_cumul, digits = 3)
 
 #"The most important output are the intrinsic rate (lambda_0), and the relative strength of social transmission (s), whose interpretations are the same as the NBDA package. The relative strength of social transmission (s = s_prime / lambda_0) is generally what we’re after. %ST for network n is reported as percent_ST[n]. This is a single-network model, thus percent_ST[1] is the estimated percentage of events that occurred through social transmission. The [1] refers to the “assoc” network, as we’ve only given a single network. If you fit a multi-network model, all networks will have an estimate. For a number of reasons, STbayes actually fits lambda_0 and social transmission rate (s_prime) on the log scale. The linear transformation of s_prime itself usually isn’t reported and is excluded from the output, but you could calculate it yourself from the fit.
 
-plot_data_obs <- get_plot_data(test_event_data)
-plot_data_ppc <- get_plot_data_ppc(fit = full_fit, data_list = data_list)
+plot_data_obs <- get_plot_data(event_data)
+plot_data_ppc <- get_plot_data_ppc(fit = fit_dynamic, data_list = data_list)
+plot_data_ppc_cumul <- get_plot_data_ppc(fit = fit_dynamic_cumul, data_list = data_list_cumul)
+
 
 # plot it
 ggplot() +
@@ -154,5 +156,15 @@ ggplot() +
             aes(x = time, y = cum_prop, 
                 group = interaction(draw, trial)), alpha = .1) +
   geom_line(data = plot_data_obs, aes(x = time, y = cum_prop), linewidth = 1) +
-  labs(x = "Time", y = "Cumulative proportion informed", color = "Trial") +
+  labs(x = "Time", y = "Cumulative proportion informed", color = "Trial",
+       title = "Dynamic network (sequential)") +
+  theme_minimal()
+
+ggplot() +
+  geom_line(data = plot_data_ppc_cumul, 
+            aes(x = time, y = cum_prop, 
+                group = interaction(draw, trial)), alpha = .1) +
+  geom_line(data = plot_data_obs, aes(x = time, y = cum_prop), linewidth = 1) +
+  labs(x = "Time", y = "Cumulative proportion informed", color = "Trial",
+       title = "Dynamic network (cumulative within days)") +
   theme_minimal()
