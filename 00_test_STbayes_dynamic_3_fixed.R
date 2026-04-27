50f376f457a34b458a6d631df8fef19641ec007e
Kaija Gahm <kaija.gahm@gmail.com>
2026-04-27 12:39:55 -0700

    remove investigation of right-censored indivs

--- 00_test_STbayes_dynamic_3_fixed.R
+++ 00_test_STbayes_dynamic_3_fixed.R
@@ -1,5 +1,4 @@
 # Testing stBayes
-
 library(STbayes)
 library(ggplot2)
 library(tidyverse)
@@ -69,9 +68,6 @@ all_indivs_sorted <- sort(unique(as.character(gps$individual_local_identifier)))
 gps_diffusion <- gps %>% filter(time_since_carcass >= 0)
 first_sightings <- get_first_sightings(gps_diffusion, hours_after_carcass, gps_spd, ddf, dds, seeds)
 
-# Get right-censored individuals
-rightcens <- all_indivs_sorted[!(all_indivs_sorted %in% c(first_sightings$individual_local_identifier, seeds))]
-
 gps_fornetwork <- gps_diffusion %>%
   filter(time_since_carcass >= 0 & time_since_carcass <= as.numeric(hours_after_carcass)) %>%
   mutate(time = as.numeric(daytime_since_carcass)*60*60) %>% # this will now correspond to the numeric times in test_event_data.
@@ -83,35 +79,20 @@ write_rds(first_sightings, file = "data/created/first_sightings_3.RDS")
 # Now we have the event data; time to format it the way that STbayes needs.
 event_data <- format_event_data_new(first_sightings, seeds, all_indivs_sorted, time_col = "daytime_since_carcass", carc = carc, gps_fornetwork = gps_fornetwork)
 
-gps_fornetwork_norightcens <- gps_fornetwork %>% filter(!(individual_local_identifier %in% rightcens))
-all_indivs_sorted_norightcens <- all_indivs_sorted[!(all_indivs_sorted %in% rightcens)]
-event_data_nocensored <- format_event_data_new(first_sightings, seeds, all_indivs_sorted_norightcens, time_col = "daytime_since_carcass", carc = carc, gps_fornetwork = gps_fornetwork_norightcens)
-
 cutpoints <- unique(event_data$time)
 
-# For right-censored individuals, need fewer cutpoints. Need to end up with 37 networks for 37 individuals. So I need 38 cutpoints.
-cutpoints_norightcens <- unique(event_data_nocensored$time)
 if(!(0 %in% cutpoints)){
   cutpoints <- c(0, cutpoints)}
-if(!(0 %in% cutpoints_norightcens)){
-  cutpoints_norightcens <- c(0, cutpoints_norightcens)}
 
 cutpoints[length(cutpoints)] <- event_data$t_end[1]
 length(cutpoints) # need 39 cutpoints so we can have 38 bins so we can define 37 events plus censored indivs.
-length(cutpoints_norightcens) # need 38 cutpoints so we can have 38 bins so we can define 37 events plus censored indivs.
 bins <- sort(unique(cut(gps_fornetwork$time, breaks = cutpoints)))
-bins_norightcens <- sort(unique(cut(gps_fornetwork_norightcens$time, breaks = cutpoints_norightcens)))
 
 gps_fornetwork$network <- cut(gps_fornetwork$time, breaks = cutpoints)
-gps_fornetwork_norightcens$network <- cut(gps_fornetwork_norightcens$time, breaks = cutpoints_norightcens)
 lvls <- levels(gps_fornetwork$network)
-lvls_norightcens <- levels(gps_fornetwork_norightcens$network)
 gps_fornetwork <- gps_fornetwork %>%
   filter(!is.na(network)) # remove NAs (after the diffusion period)
-gps_fornetwork_norightcens <- gps_fornetwork_norightcens %>%
-  filter(!is.na(network)) # remove NAs (after the diffusion period)
 missing_intervals <- levels(gps_fornetwork$network)[!(levels(gps_fornetwork$network) %in% gps_fornetwork$network)]
-missing_intervals_norightcens <- levels(gps_fornetwork_norightcens$network)[!(levels(gps_fornetwork_norightcens$network) %in% gps_fornetwork_norightcens$network)]
 
 # need to add a date for the missing intervals so the later code will work
 missing_intervals_lower <- as.numeric(str_extract(missing_intervals, "(?<=\\()[0-9]+"))
@@ -120,32 +101,18 @@ missing_intervals_upper <- as.numeric(str_extract(missing_intervals, "(?<=\\,)[0
 dates_before <- as.Date(unlist(purrr::map(missing_intervals_lower, ~{gps_fornetwork %>% filter(time < .x) %>% arrange(timestamp_il) %>% pull(date_il) %>% max()})))
 dates_after <- as.Date(unlist(purrr::map(missing_intervals_upper, ~{gps_fornetwork %>% filter(time > .x) %>% arrange(timestamp_il) %>% pull(date_il) %>% min()})))
 
-missing_intervals_lower_norightcens <- as.numeric(str_extract(missing_intervals_norightcens, "(?<=\\()[0-9]+"))
-missing_intervals_upper_norightcens <- as.numeric(str_extract(missing_intervals_norightcens, "(?<=\\,)[0-9]+(?=\\])"))
-
-dates_before_norightcens <- as.Date(unlist(purrr::map(missing_intervals_lower_norightcens, ~{gps_fornetwork_norightcens %>% filter(time < .x) %>% arrange(timestamp_il) %>% pull(date_il) %>% max()})))
-dates_after_norightcens <- as.Date(unlist(purrr::map(missing_intervals_upper_norightcens, ~{gps_fornetwork_norightcens %>% filter(time > .x) %>% arrange(timestamp_il) %>% pull(date_il) %>% min()})))
-
 # I think for now I'm just going to take the date before
 to_add <- data.frame(network = missing_intervals, date_il = dates_before) # this is super buggy and i need to return to it!
-to_add_norightcens <- data.frame(network = missing_intervals_norightcens, date_il = dates_before_norightcens) # this is super buggy and i need to return to it!
 
 if(nrow(to_add) > 0){
   gps_fornetwork <- bind_rows(gps_fornetwork, to_add)}
-if(nrow(to_add_norightcens) > 0){
-  gps_fornetwork_norightcens <- bind_rows(gps_fornetwork_norightcens, to_add_norightcens)}
 
 gps_fornetwork <- gps_fornetwork %>%
   mutate(network = factor(network, levels = lvls)) %>%
   arrange(time_since_carcass)
-gps_fornetwork_norightcens <- gps_fornetwork_norightcens %>%
-  mutate(network = factor(network, levels = lvls_norightcens)) %>%
-  arrange(time_since_carcass)
 
 gps_list <- gps_fornetwork %>% arrange(network) %>% group_split(network, .keep = TRUE)
-gps_list_norightcens <- gps_fornetwork_norightcens %>% arrange(network) %>% group_split(network, .keep = TRUE)
 length(gps_list) == length(lvls) # yay!
-length(gps_list_norightcens) == length(lvls_norightcens)
 
 tar_load(rp)
 gps_list <- map(gps_list, ~{
@@ -156,20 +123,9 @@ gps_list <- map(gps_list, ~{
   }
 })
 
-gps_list_norightcens <- map(gps_list_norightcens, ~{
-  if(nrow(.x) == 1 & all(is.na(.x$individual_local_identifier))){
-    return(.x[0,])
-  }else{
-    return(.x)
-  }
-})
-
 dynamic_networks <- map(gps_list, ~get_fl_weighted(dat = .x, dist = ddf, rp = rp, spd = gps_spd))
-dynamic_networks_norightcens <- map(gps_list_norightcens, ~get_fl_weighted(dat = .x, dist = ddf, rp = rp, spd = gps_spd))
 dynamic_networks_fixed <- fix_nets(nets = dynamic_networks, indivs = all_indivs_sorted)
-dynamic_networks_fixed_norightcens <- fix_nets(nets = dynamic_networks_norightcens, indivs = all_indivs_sorted_norightcens)
 map(dynamic_networks_fixed, dim)
-map(dynamic_networks_fixed_norightcens, dim)
 
 networks_long <- map(dynamic_networks_fixed, ~{
   out <- .x %>% rownames_to_column(var = "focal") %>%
@@ -177,25 +133,15 @@ networks_long <- map(dynamic_networks_fixed, ~{
     mutate(trial = carc_id)
   return(out)
 })
-networks_long_norightcens <- map(dynamic_networks_fixed_norightcens, ~{
-  out <- .x %>% rownames_to_column(var = "focal") %>%
-    pivot_longer(cols = -focal, names_to = "other", values_to = "flight_sri") %>%
-    mutate(trial = carc_id)
-  return(out)
-})
 
 networks_long_dynamic <- purrr::list_rbind(networks_long, names_to = "time") # this creates a numeric column for "time", which is how stbayes wants it--sequential integer values, not group names.
-networks_long_dynamic_norightcens <- purrr::list_rbind(networks_long_norightcens, names_to = "time")
 write_rds(networks_long_dynamic, file = "data/created/networks_long_dynamic_3_fixed.RDS")
-write_rds(networks_long_dynamic_norightcens, file = "data/created/networks_long_dynamic_3_fixed_norightcens.RDS")
 
 networks_long_dynamic <- readRDS("data/created/networks_long_dynamic_3_fixed.RDS")
-networks_long_dynamic_norightcens <- readRDS("data/created/networks_long_dynamic_3_fixed_norightcens.RDS")
 
 # Network must contain all individuals
 # "The networks dataframe is used as the reference for all unique IDs, thus each ID must be included at least once in either the focal or other column. If a dyad is absent, their connection is assumed to be zero."
 all(sort(unique(c(networks_long_dynamic$focal, networks_long_dynamic$other))) == all_indivs_sorted) #TRUE
-all(sort(unique(c(networks_long_dynamic_norightcens$focal, networks_long_dynamic_norightcens$other))) == all_indivs_sorted_norightcens) #TRUE
 
 # ILVs --------------------------------------------------------------------
 ## AGE
@@ -205,13 +151,6 @@ age_ilv <- gps %>%
   select(individual_local_identifier, age_2023) %>%
   distinct() %>%
   mutate(age_2023_norm = scale(age_2023, center = TRUE, scale = TRUE)[,1]) # both scaling and centering. I'm not 100% sure this is right
-
-age_ilv_norightcens <- gps %>%
-  filter(!(individual_local_identifier %in% rightcens)) %>%
-  st_drop_geometry() %>%
-  select(individual_local_identifier, age_2023) %>%
-  distinct() %>%
-  mutate(age_2023_norm = scale(age_2023, center = TRUE, scale = TRUE)[,1])
 
 ## DISTANCE FROM CARCASS
 # "For example, the value of dist_from_resource at time=1 should reflect the average distance of the individual to the resource from the start of the observation period to the first event."
@@ -233,28 +172,6 @@ dists_dyn <- map(gps_list, ~{
          mean_dist_to_carcass_norm = replace_na(mean_dist_to_carcass_norm, 0),
          mean_dist_to_carcass_squared_norm = scale(log(mean_dist_to_carcass_squared), center = T, scale = T)[,1],
          mean_dist_to_carcass_squared_norm = replace_na(mean_dist_to_carcass_squared_norm, 0)) # distance is log-transformed (to make it more normal) AND scaled/centered. Yuck!
-
-dists_dyn_norightcens <- map(gps_list_norightcens, ~{
-  step1 <- .x %>% 
-    st_drop_geometry() %>%
-    arrange(individual_local_identifier, time_since_carcass) %>%
-    group_by(individual_local_identifier) %>%
-    summarize(mean_dist_to_carcass = mean(dist_to_carcass),
-              mean_dist_to_carcass_sqared = mean_dist_to_carcass^2)
-  missing <- all_indivs_sorted_norightcens[!(all_indivs_sorted_norightcens %in% step1$individual_local_identifier)]
-  if(length(missing) > 0){
-    missing_df <- data.frame(individual_local_identifier = missing, mean_dist_to_carcass = NA, mean_dist_to_carcass_squared = NA) # keeping the values missing so we can scale, but adding the indivs
-    step2 <- bind_rows(step1, missing_df) %>%
-      filter(!is.na(individual_local_identifier))
-  }else{
-    step2 <- step1 %>% filter(!is.na(individual_local_identifier))
-  }
-  return(step2)
-}) %>% purrr::list_rbind(names_to = "time") %>%
-  mutate(mean_dist_to_carcass_norm = scale(log(mean_dist_to_carcass), center = T, scale = T)[,1],
-         mean_dist_to_carcass_norm = replace_na(mean_dist_to_carcass_norm, 0),
-         mean_dist_to_carcass_squared_norm = scale(log(mean_dist_to_carcass_squared), center = T, scale = T)[,1],
-         mean_dist_to_carcass_squared_norm = replace_na(mean_dist_to_carcass_squared_norm, 0))
 
 #Time-varying ILVs
 #Below we add distance from resource as a time-varying ILV. Similar to dynamic networks, these need to be summarized per inter-event interval. For example, the value of dist_from_resource at time=1 should reflect the average distance of the individual to the resource from the start of the observation period to the first event.
@@ -281,9 +198,6 @@ informed <- purrr::list_rbind(informed_list, names_to = "time") %>%
          prop_informed = replace_na(prop_informed, 0)) %>%
   mutate(prop_informed_norm = scale(prop_informed, scale = T, center = T)[,1])
 
-informed_norightcens <- informed %>%
-  filter(!(id %in% rightcens)) # this does include the censored individuals in calculating the roost information ILV, but I think that's appropriate.
-
 # don't need to create the equivalent of `informed` for the first day only, since by definition nobody roosted with anyone informed the day before the carcass was placed.
 
 ## Combine all the ILVs
@@ -294,24 +208,10 @@ ILV_c <- age_ilv %>%
   select(id, age) %>%
   mutate(age = replace_na(age, 0)) # set unknown ages to the mean
 
-ILV_c_norightcens <- age_ilv_norightcens %>%
-  rename("id" = individual_local_identifier,
-         age = age_2023_norm) %>%
-  select(id, age) %>%
-  mutate(age = replace_na(age, 0)) # set unknown ages to the mean
-
 ILV_tv <- dists_dyn %>%
   select("id" = individual_local_identifier,
          time, mean_dist_to_carcass_norm, mean_dist_to_carcass_squared_norm) %>%
   left_join(informed, by = c("id", "time")) %>%
-  mutate(trial = carc_id) %>%
-  select(trial, id, time, mean_dist_to_carcass_norm, mean_dist_to_carcass_squared_norm, prop_informed_norm) %>%
-  mutate(across(c("mean_dist_to_carcass_norm", "mean_dist_to_carcass_squared_norm", "prop_informed_norm"), ~replace_na(.x, 0)))
-
-ILV_tv_norightcens <- dists_dyn_norightcens %>%
-  select("id" = individual_local_identifier,
-         time, mean_dist_to_carcass_norm, mean_dist_to_carcass_squared_norm) %>%
-  left_join(informed_norightcens, by = c("id", "time")) %>%
   mutate(trial = carc_id) %>%
   select(trial, id, time, mean_dist_to_carcass_norm, mean_dist_to_carcass_squared_norm, prop_informed_norm) %>%
   mutate(across(c("mean_dist_to_carcass_norm", "mean_dist_to_carcass_squared_norm", "prop_informed_norm"), ~replace_na(.x, 0)))
@@ -328,21 +228,12 @@ data_list <- import_user_STb(event_data = event_data,
                              ILVs = c("age", "prop_informed_norm")) 
 write_rds(data_list, file="data/data_lists/datalist_3_fixed.RDS")
 
-data_list_norightcens <- import_user_STb(event_data = event_data_nocensored, 
-                                         networks = networks_long_dynamic_norightcens,
-                                         network_type = "undirected",
-                                         ILV_c = ILV_c_norightcens,
-                                         ILV_tv = ILV_tv_norightcens,
-                                         ILVi = c("age", "mean_dist_to_carcass_norm", "prop_informed_norm"),
-                                         ILVs = c("age", "prop_informed_norm")) 
-write_rds(data_list_norightcens, file="data/data_lists/datalist_3_fixed_norightcens.RDS")
-
 # Models
 model_full_dynamic <- generate_STb_model(data_list, gq = T, est_acqTime = T)
 write(model_full_dynamic, file="data/stan_models/model_3_fixed.stan")
 
-model_full_dynamic_norightcens <- generate_STb_model(data_list_norightcens, gq = T, est_acqTime = T)
-write(model_full_dynamic_norightcens, file="data/stan_models/model_3_fixed_norightcens.stan")
+model_3_weibull <- generate_STb_model(data_list, gq = T, est_acqTime = T, intrinsic_rate = "weibull")
+write(model_3_weibull, file="data/stan_models/model_3_weibull.stan")
 
 # fit_dynamic <- fit_STb(data_list,
 #                        model_full_dynamic,
@@ -354,15 +245,17 @@ write(model_full_dynamic_norightcens, file="data/stan_models/model_3_fixed_norig
 # STb_save(fit_dynamic, output_dir = "data/cmdstan_saves", name="dynamic_daylight_ilvs3_fixed")
 fit_dynamic <- readRDS('data/cmdstan_saves/dynamic_daylight_ilvs3_fixed.rds') 
 # 
-# fit_dynamic_norightcens <- fit_STb(data_list_norightcens,
-#                                    model_full_dynamic_norightcens,
-#                                    parallel_chains = 3,
-#                                    chains = 3,
-#                                    cores = 3,
-#                                    iter = 500,
-#                                    refresh=50)
-# STb_save(fit_dynamic_norightcens, output_dir = "data/cmdstan_saves", name="dynamic_daylight_ilvs3_fixed_norightcens")
-fit_dynamic_norightcens <- readRDS('data/cmdstan_saves/dynamic_daylight_ilvs3_fixed_norightcens.rds')
+
+fit_weibull <- fit_STb(data_list,
+                       model_3_weibull,
+                       parallel_chains = 3,
+                       chains = 3,
+                       cores = 3,
+                       iter = 500,
+                       refresh=50)
+STb_save(fit_weibull, output_dir = "data/cmdstan_saves", name="dynamic_daylight_ilvs3_weibull")
+fit_weibull <- readRDS('data/cmdstan_saves/dynamic_daylight_ilvs3_weibull.rds') 
+# 
 
 model_asoc = generate_STb_model(data_list, model_type="asocial", gq = T, est_acqTime = T)
 # asocial_fit = fit_STb(data_list,
@@ -376,9 +269,7 @@ model_asoc = generate_STb_model(data_list, model_type="asocial", gq = T, est_acq
 asocial_fit <- readRDS('data/cmdstan_saves/dynamic_daylight_ilvs_asoc3_fixed.rds') 
 
 loo_output <- STb_compare(fit_dynamic, asocial_fit, method="loo-psis")
-
 comparison_df <- as.data.frame(loo_output$comparison)
-
 comparison_df$model <- rownames(comparison_df)
 
 ggplot(comparison_df, aes(x = reorder(model, elpd_diff), y = elpd_diff)) +
@@ -390,15 +281,6 @@ ggplot(comparison_df, aes(x = reorder(model, elpd_diff), y = elpd_diff)) +
   theme_minimal()+
   theme(text = element_text(size = 18))
 
-ggplot(comparison_df, aes(x = 1, y = elpd_diff, color = model)) +
-  geom_point(size = 4) + #elpd_diff
-  geom_errorbar(aes(ymin = elpd_diff - se_diff, 
-                    ymax = elpd_diff + se_diff), width = 0.1, linewidth = 2) + #SE of elpd diff
-  labs(x = "Model", y = "ELPD Difference", title = "Carcass 3 (4420641)") +
-  theme_minimal()+
-  theme(text = element_text(size = 18))+
-  coord_flip()
-
 # PSIS-LOO is an approximation of LOO, and observations with pareto-k diagnostic values >.7 may indicate that the approximation is unreliable. The function above will warn you if that is the case, and you can visually inspect these diagnostics like so:
 pareto_df = as.data.frame(loo_output$pareto_diagnostics)
 
@@ -408,11 +290,10 @@ ggplot(pareto_df, aes(x=observation, y=pareto_k, color=model))+
   geom_hline(yintercept = 0.7, linetype="dashed", color="orange")+
   geom_hline(yintercept = 1, linetype="dashed", color="red")+
   labs(x="Observation", y="Pareto-k value", title="Pareto-k diagnostics")+
-  theme_minimal() # a few high--not great. Worse than squared.
+  theme_minimal() # looks nice
 
 # SUMMARIES
 summ <- STb_summary(fit_dynamic, digits = 3)
-summ_norightcens <- STb_summary(fit_dynamic_norightcens, digits = 3)
 
 summ %>% filter(grepl("beta_", Parameter)) %>%
   select(Parameter, Median, CI_Lower, CI_Upper) %>%
@@ -437,9 +318,7 @@ summ %>% filter(grepl("beta_", Parameter)) %>%
   theme(text = element_text(size = 18))# so, mean dist has a huge impact in this model, but when we square it, its impact goes away almost entirely. Why?
 
 plot_data_obs <- get_plot_data(event_data)
-plot_data_obs_norightcens <- get_plot_data(event_data_nocensored)
 plot_data_ppc <- get_plot_data_ppc(fit = fit_dynamic, data_list = data_list)
-plot_data_ppc_norightcens <- get_plot_data_ppc(fit = fit_dynamic_norightcens, data_list = data_list_norightcens)
 
 # plot it
 ggplot() +
@@ -449,13 +328,4 @@ ggplot() +
   geom_line(data = plot_data_obs, aes(x = time, y = cum_prop), linewidth = 1) +
   labs(x = "Time", y = "Cumulative proportion informed", color = "Trial",
        title = "Original") +
-  theme_minimal()
-
-ggplot() +
-  geom_line(data = plot_data_ppc_norightcens, 
-            aes(x = time, y = cum_prop, 
-                group = interaction(draw, trial)), alpha = .1) +
-  geom_line(data = plot_data_obs_norightcens, aes(x = time, y = cum_prop), linewidth = 1) +
-  labs(x = "Time", y = "Cumulative proportion informed", color = "Trial",
-       title = "No right-censored indivs") +
   theme_minimal()
